333a0a1fa3af197afd16d785bbf25c36501d0ec1
Akshay Gosavi <gosavia.gosavi@gmail.com>
2024-04-04 00:59:34 +0530

    feat: added which-key plugin

--- lua/akshay/remap.lua
+++ lua/akshay/remap.lua
@@ -1,12 +1,12 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv" , vim.cmd.Ex)
-vim.keymap.set('i', 'kj', '<Esc>')
+vim.g.mapleader = ' '
+vim.keymap.set('n', '<leader>pv' , vim.cmd.Ex, { desc = 'Open Netrw file explorer' })
+vim.keymap.set('i', 'kj', '<Esc>', { desc = 'Return to Normal mode' })
 -- center the search result + and open any folds to see the cursor line
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', '<leader>w', vim.cmd.write) 
+vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Center next search result and open any folds' })
+vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Center previous search result and open any folds' })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Move down a screen and center cursor' })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Move up a screen and center cursor' })
+vim.keymap.set('n', '<leader>w', vim.cmd.write, { desc = ' Save changes' })
 -- add a mark to 'z' register + join two lines + move cursor to mark z
 -- this will join current line with line above and keep cursor on same position:
-vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "J", "mzJ`z", { desc = ' Join current line with line above and keep cursor on same position' })
