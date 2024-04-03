2fcf9d20d1a4f2a1baffe33b388aed9e6d75bbf3
Akshay Gosavi <gosavia.gosavi@gmail.com>
2024-04-03 16:04:29 +0530

    update: section a now shows macro related status

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,7 +1,12 @@
 require('lualine').setup({
-	options = {
-		disabled_filetypes = {
-			statusline = { 'NvimTree' },
-		}
-	}
+  options = {
+    disabled_filetypes = {
+      statusline = { 'NvimTree' },
+    },
+  },
+  sections = {
+    lualine_a = {
+      'mode' , require('noice').api.statusline.mode.get
+    },
+  },
 })
