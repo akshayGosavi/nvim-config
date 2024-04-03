dd17b4d51ecb70c927ba49c043a534fd3cdb0bb8
Akshay Gosavi <gosavia.gosavi@gmail.com>
2024-04-03 16:01:56 +0530

    chore: retab

--- after/plugin/noice.lua
+++ after/plugin/noice.lua
@@ -1,9 +1,9 @@
 require('noice').setup({
-	lsp = {
-		-- override markdown rendering so other plugins use 'treesitter'
-		override = {
-			['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-			['vim.lsp.util.stylize_markdown'] = true,
-		},
-	},
+  lsp = {
+    -- override markdown rendering so other plugins use 'treesitter'
+    override = {
+      ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+      ['vim.lsp.util.stylize_markdown'] = true,
+    },
+  },
 })
