ef9db50f64428604f6852dd95b8d4a864f488a95
Akshay Gosavi <gosavia.gosavi@gmail.com>
2024-04-07 17:23:05 +0530

    update: git keymaps added

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,3 +4,7 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[G]rep search' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find [B]uffers' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Find [H]elp' })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Find [G]it Files' })
+-- git actions with telescope
+vim.keymap.set('n', '<leader>gst', builtin.git_status, { desc ='Git Status' });
+vim.keymap.set('n', '<leader>gsh', builtin.git_stash, { desc ='Git Stash' });
+
