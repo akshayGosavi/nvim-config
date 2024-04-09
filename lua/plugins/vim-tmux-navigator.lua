e36097c408c30a58f2192582f4827a42b62a6559
Akshay Gosavi <gosavia.gosavi@gmail.com>
2024-04-10 00:19:35 +0530

    feat: adding tmux compatibility

---
+++ lua/plugins/vim-tmux-navigator.lua
@@ -0,0 +1,15 @@
+return {
+  'christoomey/vim-tmux-navigator',
+  -- cmd = {
+  --   'TmuxNavigateLeft',
+  --   'TmuxNavigateRight',
+  --   'TmuxNavigateUp',
+  --   'TmuxNavigateDown',
+  -- },
+  -- keys = {
+  --   { 'C-h', '<cmd><C-U>TmuxNavigateLeft<cr>'},
+  --   { 'C-j', '<cmd><C-U>TmuxNavigateDown<cr>'},
+  --   { 'C-k', '<cmd><C-U>TmuxNavigateUp<cr>'},
+  --   { 'C-l', '<cmd><C-U>TmuxNavigateRight<cr>'},
+  -- }
+}
