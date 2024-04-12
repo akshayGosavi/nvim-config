0b2064bd3d394e9e9887c155baa6cea59d90d9e0
Akshay Gosavi <gosavia.gosavi@gmail.com>
2024-04-12 21:25:29 +0530

    update: boyscout + build on TSUpdate for treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,16 @@
 require('nvim-treesitter.configs').setup({
-	ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'javascript', 'html', 'css', 'java', 'properties', 'ruby', 'sql', 'typescript', 'xml' },
-	sync_install = false,
-	auto_install = true,
-	highlight = { enabled = true },
+  ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'javascript', 'html', 'css', 'java', 'properties', 'ruby', 'sql', 'typescript', 'xml' },
+  sync_install = false,
+  auto_install = true,
+  highlight = { enabled = true },
+  indent = { enabled = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<C-space>',
+      node_incremental = '<C-space>',
+      scope_incremental = false,
+      node_decremental = '<bs>',
+    }
+  }
 })
